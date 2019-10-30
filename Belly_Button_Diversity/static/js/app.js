9763faac225542dbd32a1a9abf98718dca31016b
toddhnstl <50678522+toddhnstl@users.noreply.github.com>
2019-10-30 17:57:54 -0500

    updates, plots on page.  Bubble chart doesnt have colors

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,27 +3,141 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then(function(sampleResult) {
+    //console.log(sampleResult);
+    
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    smplRsltsHtml = d3.select("#sample-metadata");
+    
     // Use `.html("") to clear any existing metadata
+    smplRsltsHtml.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    // sampleResult.forEach(function(sampleResult) {
+    //   console.log(sampleResult);
+    //   //var row = tbody.append("tr");
+    
+      Object.entries(sampleResult).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        // in the weather report object
+        var p = smplRsltsHtml.append("p");
+        p.text(`${key}: ${value}`)
+      });
+    
+
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+    console.log(`WFEQ: ${sampleResult["WFREQ"]}`);
+
+    // var data = [
+    //   {
+    //     domain: { x: [0, 1], y: [0, 1] },
+    //     value: sampleResult["WFREQ"],
+    //     title: { text: "Wash Frequency" },
+    //     type: "indicator",
+    //     mode: "gauge+number",
+    //     gauge: {
+    //       axis: { range: [null, 9]}
+    //     }
+    //   }
+    // ];
+
+    
+    //var layout = { width: 300, height: 250, margin: { t: 0, b: 0 } };
+    //Plotly.newPlot(gauge, data, layout);
+    //Plotly.newPlot(gauge, data)
+
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: sampleResult["WFREQ"],
+        title: { text: "Wash Frequency" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        //delta: { reference: 7 },
+        gauge: {
+          axis: { range: [null, 9] },
+          steps: [
+            { range: [0, 1], color: "#FFF8DC" },
+            { range: [1, 2], color: '#FFF8DC' },
+            { range: [2, 3], color: "#FFEBCD" },
+            { range: [3, 4], color: "#FFDEAD" },
+            { range: [4, 5], color: "#90EE90" },
+            { range: [5, 6], color: "#32CD32" },
+            { range: [6, 7], color: "#8FBC8F" },
+            { range: [7, 8], color: '#3CB371' }, 
+            { range: [8, 9], color: '#2E8B57' },
+          ]
+          // threshold: {
+          //   line: { color: "red", width: 4 },
+          //   thickness: 0.75,
+          //   value: 8
+          // }
+        }
+      }
+    ];
+    
+    var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot(gauge, data, layout);
+
+  }); // end d3.json(`/metadata/${sample}`).then(function(sampleResult)
+   
 }
 
 function buildCharts(sample) {
-
+  ///  @app.route("/samples/<sample>")
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  d3.json(`/samples/${sample}`).then(function(samples) {
+    //console.log(samples);
     // @TODO: Build a Bubble Chart using the sample data
+    var trace1 = {
+      x: samples["otu_ids"],
+      y: samples["sample_values"],
+      mode: 'markers',
+      text: samples["otu_labels"],
+      marker: {
+        size: samples["sample_values"],
+        color: samples["out_ids"]
+      }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: '',
+      showlegend: false,
+      height: 600,
+      width: 800
+    };
+    
+    Plotly.newPlot('bubble', data, layout);
+
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var trace1 = {
+      labels: samples["otu_ids"].slice(0, 10),
+      values: samples["sample_values"].slice(0, 10),
+      hoverinfo: samples["otu_labels"].slice(0,10),
+      textinfo: 'none',
+      type: 'pie'
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: "Top Ten",
+    };
+
+    Plotly.newPlot("pie", data, layout);
+
+
+  }); // End of d3.json(`/samples/${sample}`).then(function(samples)
 }
 
 function init() {
