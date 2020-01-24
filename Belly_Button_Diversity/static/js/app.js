01eece896019260fab88a52190826ca5ca17a463
toddhnstl <50678522+toddhnstl@users.noreply.github.com>
2020-01-23 18:56:52 -0600

    update app.js do some cleanup

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,12 +3,12 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-  d3.json(`/metadata/${sample}`).then(function(sampleResult) {
+  d3.json(`/metadata/${sample}`).then(function (sampleResult) {
     //console.log(sampleResult);
-    
+
     // Use d3 to select the panel with id of `#sample-metadata`
     smplRsltsHtml = d3.select("#sample-metadata");
-    
+
     // Use `.html("") to clear any existing metadata
     smplRsltsHtml.html("");
 
@@ -18,33 +18,32 @@ function buildMetadata(sample) {
     // sampleResult.forEach(function(sampleResult) {
     //   console.log(sampleResult);
     //   //var row = tbody.append("tr");
-    
-      Object.entries(sampleResult).forEach(function([key, value]) {
-        //console.log(key, value);
-        // Append a cell to the row for each value
-        // in the weather report object
-        var p = smplRsltsHtml.append("p");
-        p.text(`${key}: ${value}`)
-      });
-    
+
+    Object.entries(sampleResult).forEach(function ([key, value]) {
+      //console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var p = smplRsltsHtml.append("p");
+      p.text(`${key}: ${value}`)
+    });
+
 
 
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-    //console.log(`WFEQ: ${sampleResult["WFREQ"]}`);
-
     var data = [
       {
-        domain: { x: [0, 9], y: [0, 9]},
+        domain: { x: [0, 9], y: [0, 9] },
         value: sampleResult["WFREQ"],
         title: { text: "Scrubs per Week" },
         type: "indicator",
         mode: "gauge+number",
         //delta: { reference: 7 },
         gauge: {
-          axis: { range: [null, 9], tickwidth: 3, 
-            tickcolor: "black", tick0: 1, dtick: 1 },
-          bar: {'color': "red"},
+          axis: {
+            range: [null, 9], tickwidth: 3,
+            tickcolor: "black", tick0: 1, dtick: 1
+          },
+          bar: { 'color': "red" },
           steps: [
             { range: [0, 1], color: "#CDE4C9" },
             { range: [1, 2], color: '#BED4BB' },
@@ -52,45 +51,40 @@ function buildMetadata(sample) {
             { range: [3, 4], color: "#769A70" },
             { range: [4, 5], color: "#6AA860" },
             { range: [5, 6], color: "#67B45A" },
-            { range: [6, 7], color: "#5CB64D" }, 
-            { range: [7, 8], color: '#478F3B' }, 
+            { range: [6, 7], color: "#5CB64D" },
+            { range: [7, 8], color: '#478F3B' },
             { range: [8, 9], color: '#35692D' },
           ]
-          // threshold: {
-          //   line: { color: "red", width: 4 },
-          //   thickness: 0.75,
-          //   value: 8
-          // }
         }
       }
     ];
-    
-    var layout = { 
-  
+
+    var layout = {
+
       title: {
-          text:'Belly Button Washing Frequency',
-          font: {
-            family: 'Courier Bold',            
-            Style: 'bold',
-            size: 24
-          },
-          xref: 'paper',
-          x: 0.01,
+        text: 'Belly Button Washing Frequency',
+        font: {
+          family: 'Courier Bold',
+          Style: 'bold',
+          size: 24
         },
-      width: 450, 
-      height: 300, 
-      margin: { t: 45, b: .05 } 
+        xref: 'paper',
+        x: 0.01,
+      },
+      width: 450,
+      height: 300,
+      margin: { t: 45, b: .05 }
     };
     Plotly.newPlot(gauge, data, layout);
 
   }); // end d3.json(`/metadata/${sample}`).then(function(sampleResult)
-   
+
 }
 
 function buildCharts(sample) {
   ///  @app.route("/samples/<sample>")
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(`/samples/${sample}`).then(function(samples) {
+  d3.json(`/samples/${sample}`).then(function (samples) {
     //console.log(samples);
     // @TODO: Build a Bubble Chart using the sample data
 
@@ -104,11 +98,11 @@ function buildCharts(sample) {
 
 
     //console.log(`labels in temp array?: ${tmpArray}`)
-    scaledOut_Ids = tmpArray.map(function(value){
+    scaledOut_Ids = tmpArray.map(function (value) {
       return grnScale(value);
-    }); 
-    
-    var myColors = scaledOut_Ids.map(function(value){
+    });
+
+    var myColors = scaledOut_Ids.map(function (value) {
       return `rgb(0, ${value}, 0)`
     })
 
@@ -124,26 +118,24 @@ function buildCharts(sample) {
         color: myColors
       }
     };
-    
+
     var data = [trace1];
-    
+
     var layout = {
       title: '',
       showlegend: false,
       height: 600,
       width: 1000
     };
-    
+
     Plotly.newPlot('bubble', data, layout);
 
 
     // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
     var trace1 = {
       labels: samples["otu_ids"].slice(0, 10),
       values: samples["sample_values"].slice(0, 10),
-      hoverinfo: samples["otu_labels"].slice(0,10),
+      hoverinfo: samples["otu_labels"].slice(0, 10),
       textinfo: 'none',
       type: 'pie'
     };
